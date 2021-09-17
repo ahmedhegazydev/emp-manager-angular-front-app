e8503c4eec74babe7ff16aba6cdad1fed57ea45c
ahmedhegazy <ahmed.hegazy@safavisa.com>
2021-09-17 18:11:26 +0200

    finishing crud operations for employee

---
+++ src/app/employee.ts
@@ -0,0 +1,17 @@
+
+export class Employee{
+
+  id: number = 0;
+  name: String = "";
+  email: String = "";
+  jobTitle: String = "";
+  employeeCode: String = "";
+  phone: String = "";
+  imageUrl: String = "";
+
+
+  constructor() {
+    // this.name = name;
+  }
+
+}
